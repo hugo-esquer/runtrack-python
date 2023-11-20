8e95f111128446303489e0923338d5fc2cd703aa
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-20 18:10:34 +0100

    correction

--- jour01/job10/job10.py
+++ jour01/job10/job10.py
@@ -1,12 +1,13 @@
-montant = 0
-rendement = 0
+montant = 1500
+rendement = 15
 gain = (montant*rendement) / 100
-print(gain)
-montant += 5_000
+print(f"Les gains sont de {gain}")
+montant += 5_000 + gain
 rendement += 2
 gain = (montant*rendement) / 100
-print(gain)
+print(f"Les nouveaux gains sont de {gain}")
+montant = montant + gain
 montant = montant - ((montant*10)/100)
 rendement -= 1
 gain = (montant*rendement) / 100
-print(gain)
+print(f"Le montant des gain finaux est de {gain}")
--- jour01/plus loin/plus_loin.py
+++ jour01/plus loin/plus_loin.py
@@ -1,8 +1,9 @@
 chaine = input ("Enter un mot ou un phrase: ")
-print(chaine)
-if "e" in chaine:
-    print ("contient un e !")
+if "e" in chaine and "E" in chaine:
+    print ("contient un e et E !")
 elif "E" in chaine:
     print("contient un E !")
+elif "e" in chaine:
+    print("contient un e !")
 else:
-    print("Ne contient pas de e")
+    print("Ne contient pas de e ni de E")
