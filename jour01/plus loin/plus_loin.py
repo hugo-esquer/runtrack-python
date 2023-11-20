5fbea67384c1c11806712dad6712bb19b256b3ea
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-20 12:14:32 +0100

    fin du allez plus loin

---
+++ jour01/plus loin/plus_loin.py
@@ -0,0 +1,8 @@
+chaine = input ("Enter un mot ou un phrase: ")
+print(chaine)
+if "e" in chaine:
+    print ("contient un e !")
+elif "E" in chaine:
+    print("contient un E !")
+else:
+    print("Ne contient pas de e")
