16cad37ae6fe876e7671ff3a32df67d2db669f40
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-21 19:52:37 +0100

    push job06 et 07 correction job 04 et 05

--- jour02/job04/job04.py
+++ jour02/job04/job04.py
@@ -2,5 +2,5 @@ N = int(input("Entez un entier supperieur à zéro: "))
 for i in range(1, N+1):
     print(f"Table de multiplication de {i}")
     for j in range(1, 10+1):
-        print(f"{i} x {j} = " + str(i*j))
+        print(f"{i} x {j} = {i*j}")
     print("\n")
