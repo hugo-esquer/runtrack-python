7ad8b6075dd939cbf290502ecac5b1fd61864ddc
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-22 11:57:40 +0100

    job06

---
+++ jour03/job06/job06.py
@@ -0,0 +1,11 @@
+def plus_ou_moins_0(nombre):
+    if nombre > 0:
+        print("positif")
+    elif nombre < 0:
+        print("négatif")
+    else:
+        print("Zéro n'est ni positif ni négatif")
+
+plus_ou_moins_0(-6)
+plus_ou_moins_0(105)
+plus_ou_moins_0(0)
