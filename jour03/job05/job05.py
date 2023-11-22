99caf30f79149e86d34f527437e639060881ee9f
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-22 11:40:58 +0100

    job05

--- jour03/job05/job05.py
+++ jour03/job05/job05.py
@@ -1,1 +1,20 @@
-def calcule
+def calcule(num1, operateur, num2):
+    if operateur == "x":
+        return num1*num2
+    elif operateur == "+":
+        return num1+num2
+    elif operateur == "/":
+        return num1/num2
+    elif operateur == "-":
+        return num1-num2
+    elif operateur == "%":
+        return num1%num2
+    else:
+        return "Entrer un opérateur valide"
+    
+print(calcule(18, "x", 32))
+print(calcule(65, "+", 5))
+print(calcule(70, "/", 5))
+print(calcule(75, "-", 6))
+print(calcule(635, "%", 36))
+print(calcule(56, "v", 67))
