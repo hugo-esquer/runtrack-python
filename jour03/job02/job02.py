ff9db27cb5731c4de1be6e23309c0012a8138b9f
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-22 11:12:07 +0100

    job01 à 03

---
+++ jour03/job02/job02.py
@@ -0,0 +1,7 @@
+def My_print_name(name):
+    print(name)
+
+My_print_name("Hugo")
+My_print_name("Erika")
+My_print_name("Kheira")
+My_print_name("Pierre")
---
+++ jour03/job03/Job03.py
@@ -0,0 +1,6 @@
+def Add(a, b):
+    print(a + b)
+
+Add(2, 9)
+Add(5, 18)
+Add(42, 42)
