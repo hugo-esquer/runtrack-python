adc07f970cbefd49d63f4df0f0e98e066b1b86b9
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-23 19:24:27 +0100

    correction job12

--- jour04/job12/main.py
+++ jour04/job12/main.py
@@ -1,17 +1,23 @@
 L=[76, 8, 43, 90, 13]
 
-def minimum(liste):
-    num_min=liste[0]
+def long(liste):
+    count=0
     for i in liste:
-        if i < num_min:
-            num_min=i
-    return num_min
+        count+=1
+    return count
 
+def organiser(liste):
+    i = long(liste) - 1
+    while i > 0:
+        j=0
+        while j < i:
+            if liste[j+1]<liste[j]:
+                stock=liste[j+1]
+                liste[j+1]=liste[j]
+                liste[j]=stock
+            j+=1
+        i-=1
 
-sorted_list=[]
-while L:
-    sorted_list.append(minimum(L))
-    L.remove(minimum(L))
-
-L=sorted_list
+print(L)
+organiser(L)
 print(L)
