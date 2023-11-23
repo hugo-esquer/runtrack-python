486a2e986c8af639f3f4dcee17831ebece8da0df
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-23 20:01:51 +0100

    job 13

--- jour04/job13/main.py
+++ jour04/job13/main.py
@@ -1,8 +1,17 @@
 L=[10,20,30,20,10,50,60,40,80,50,40]
-L_unique=[]
-for i in L:
-    if i not in L_unique:
-        L_unique.append(i)
+def long(liste):
+    count=0
+    for i in liste:
+        count+=1
+    return count
 
-L=L_unique
+def doublons(liste):
+    i=0
+    while i < long(liste):
+        if liste[i] in liste[i+1:]:
+            liste[i:i+1]=[]
+        i+=1
+
+print(L)
+doublons(L)
 print(L)
