772860b0d9eb35f0c131244b38ff7ee79fdb737f
hugo-esquer <hugo.esquer@laplateforme.io>
2023-11-27 16:25:18 +0100

    job05 et 08

--- jour05/job02/main.py
+++ jour05/job02/main.py
@@ -2,8 +2,10 @@ def rectangle(width,height):
     i=0
     if width < 3 or height < 3:
         return print("Erreur: requiers des valeurs supérieur ou égale à 3")
+    
     cotes_h_b = "|"+("-"*(width-2))+"|"
     cotes_g_d = "|"+(" "*(width-2))+"|"
+
     print(cotes_h_b)
     while i < (height-2):
         print(cotes_g_d)
---
+++ jour05/job08/main.py
@@ -0,0 +1,21 @@
+L = [89, 67, 22, 11, 96, 59, 42]
+
+def my_sort(liste):
+    # definir un compteur d'action
+    count = 0
+    for i in range(len(liste)):
+        # mémoriser liste[i] dans une variable
+        x = liste[i]
+
+        # décaler les éléments liste[0]...liste[i-1] qui sont plus grand que x, en partant de liste[i-1]
+        j=i
+        while j > 0 and liste[j-1] > x: # parcourir la liste en sens inverse et décaler les chiffre plus grand que x
+            liste[j] = liste[j-1]       # décaler [j+1] tant qu'il est plus grand que x
+            j = j-1                     # pour continuer à verifier les éléments lors de la boucle suivante
+            count+=1                    # compte des actions
+        liste[j] = x                    # on "place" l'élément en mémoire dans le "trou"
+
+    print(f"Nombre total de coups nécessaire pour trier la liste: {count}")
+    print(liste)
+
+my_sort(L)
